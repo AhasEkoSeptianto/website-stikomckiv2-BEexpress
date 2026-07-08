a7fbb00ebd1ec475bbbb26055c911ecf04cbf4fb
unknown <ahaseko1@gmail.com>
2026-07-08 23:25:38 +0700

    test

---
+++ functions/api.js
@@ -0,0 +1,17 @@
+import express from "express";
+import serverlessHttp from "serverless-http";
+
+const app = express();
+
+app.get("/.netlify/functions/api", (req, res) => {
+  return res.json({
+    message: "hello world",
+  });
+});
+
+const handler = serverlessHttp(app);
+
+module.exports.handler = async (event, context) => {
+  const result = await handler(event, context);
+  return result;
+};
