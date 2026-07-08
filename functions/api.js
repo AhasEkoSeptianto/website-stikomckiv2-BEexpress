819bb104e079f67fd25cbf0b300526fe457810ec
unknown <ahaseko1@gmail.com>
2026-07-08 23:52:06 +0700

    fix

--- functions/api.js
+++ functions/api.js
@@ -1,12 +1,55 @@
 import express from "express";
 import serverlessHttp from "serverless-http";
-import { app } from "./../server";
+// import { app } from "./../server";
 
-app.get("/.netlify/functions/api", (req, res) => {
-  return res.json({
-    message: "hello world",
-  });
-});
+// app.get("/.netlify/functions/api", (req, res) => {
+//   return res.json({
+//     message: "hello world",
+//   });
+// });
+
+// const fileUpload = require("express-fileupload");
+import fileUpload from "express-fileupload";
+import cors from "cors";
+// const cors = require("cors");
+import bodyParser from "body-parser";
+// const bodyParser = require("body-parser");
+import jwt from "jsonwebtoken";
+// const jwt = require("jsonwebtoken");
+
+import dotenv from "dotenv";
+dotenv.config();
+
+// require("dotenv").config();
+
+import "./../models/connect_monggoatlas.js";
+// require("./models/connect_monggoatlas");
+import "./../config/myMediaGDrive.js";
+// require("./config/myMediaGDrive");
+
+// midleware
+app.use(bodyParser.json());
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  }),
+);
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded());
+app.use(express.static("public"));
+app.use(
+  fileUpload({
+    useTempFiles: true,
+    tempFileDir: "/tmp/",
+  }),
+);
+
+app.use("/.netlify/functions/api", require("./routers/api/route"));
+
+// module.exports = { app };
+// // app.listen(port, () => console.log("running at port 8080"));
 
 const handler = serverlessHttp(app);
 
